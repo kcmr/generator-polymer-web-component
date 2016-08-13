1608ab609aed1f159d7f7af068d5f575b69dcd47
Kus Cámara <kcmr@users.noreply.github.com>
2016-08-13 22:18:26 +0200

    chore(package, generator): remove unused dependencies

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,6 @@
 'use strict';
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
-var yosay = require('yosay');
 var path = require('path');
 var mkdirp = require('mkdirp');
 var _ = require('lodash-addons');
